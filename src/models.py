0d747cf9eed7381256246ce40fb3afd6a98ae951
timaracov <neyenburgz@mail.ru>
2021-12-10 01:12:57 +0200

    moddify models

--- src/models.py
+++ src/models.py
@@ -1,34 +1,42 @@
 from enum import unique
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, Table
 
 db = SQLAlchemy()
 
 class Cards(db.Model):
-    id = db.Column(db.Integer, unique=True, primary_key=True) # здесь должен быть uuid
+    __tablename__ = 'Cards'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     enabled = db.Column(db.Boolean, default=True)
 
 
-class enter_log(db.Model):
+class EnterLog(db.Model):
+    __tablename__ = 'LogEnter'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
 
 class Facilities(db.Model):
+    __tablename__ = 'Facilities'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String())
 
 
 class FacilityBooking(db.Model):
+    __tablename__ = 'FacilityBooking'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     from_time = db.Column(db.DateTime, unique=True)
     to_time = db.Column(db.DateTime, unique=True)
 
 
 class FacilityType(db.Model):
+    __tablename__ = 'FacilityType'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(), unique=True)
 
 
 class News(db.Model):
+    __tablename__ = 'News'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text())
     annotation = db.Column(db.Text())
@@ -36,6 +44,7 @@ class News(db.Model):
 
 
 class UserAudit(db.Model):
+    __tablename__ = 'UserAudit'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     action = db.Column(db.String(), nullable=False)
     model = db.Column(db.String(), nullable=False)
@@ -43,8 +52,16 @@ class UserAudit(db.Model):
 
 
 class Users(db.Model):
+    __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(), nullable=True)
     email = db.Column(db.String(), nullable=False, unique=True)
+    email_confirm = db.Column(db.Boolean, unique=False, default=False)
     password = db.Column(db.String(), nullable=False)
+
+
+users_news_table = Table('UsersToNews', db.metadata, 
+    db.Column('news_id', ForeignKey('News.id'), primary_key=True),
+    db.Column('author_id', ForeignKey('Users.id'), primary_key=True),
+)
 
