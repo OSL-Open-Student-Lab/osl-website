fa0dfa9aabfa6f902f536f69681d6ce702bff663
timaracov <neyenburgz@mail.ru>
2021-12-10 01:14:09 +0200

    temp stuff for setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from flask_session import Session
 from flask_mail import Mail
 
 from src import app
-from src.blueprints import auth, query, home
+from src.blueprints import auth, home
 from src.models import db
 
 
@@ -12,7 +12,10 @@ Mail(app)
 Session(app)
 
 app.register_blueprint(auth.auth_bp)
-app.register_blueprint(query.query_bp)
 app.register_blueprint(home.home_bp)
+
+# with app.app_context():
+#     db.create_all()
+
 
 app.run(debug=True)
