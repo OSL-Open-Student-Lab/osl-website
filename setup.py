f0cdd99e591ab5b878a6b847ac28d371547b8323
timaracov <neyenburgz@mail.ru>
2021-12-15 03:44:46 +0200

    setup added

---
+++ setup.py
@@ -0,0 +1,18 @@
+from src import app
+from src.models import db
+from src.blueprints import auth, query, home
+from flask_session import Session
+
+db.init_app(app)
+Session(app)
+
+
+# with app.app_context():
+#     db.create_all()
+
+if __name__ == '__main__':
+    app.register_blueprint(auth.auth_bp)
+    app.register_blueprint(query.query_bp)
+    app.register_blueprint(home.home_bp)
+    
+    app.run(debug=True)
