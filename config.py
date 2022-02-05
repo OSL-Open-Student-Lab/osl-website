7c51321c5c1a6103b455caea0f5d17a1fb7bb68f
timaracov <neyenburgz@mail.ru>
2022-02-05 19:12:27 +0200

    updating procfile.txt

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
-from src.models import db
+import os
 import datetime
+
+from src.models import db
+
+SECRET_KEY = 'dev_key'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 SESSION_TYPE = 'sqlalchemy'
 SESSION_SQLALCHEMY = db
 PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=7)
-SECRET_KEY = 'dev_key'
 REMEMBER_COOKIE_DURATION=datetime.timedelta(days=7)
 REMEMBER_COOKIE_HTTPONLY=True
