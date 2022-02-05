5f21ad4cebf7cea1025b51184b9086fec7808566
timaracov <neyenburgz@mail.ru>
2022-02-05 19:47:36 +0200

    testing heroku

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from src.models import db
 
 SECRET_KEY = 'dev_key'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/site.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/site.db'
+SQLALCHEMY_DATABASE_URI = 'postgres://lwhyaywahcvplq:cad4f8035162923ea3e2deeaa0ac233f9c921fc7eaaf3415cff72451895ea821@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dekaee32lbf2ro'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SESSION_TYPE = 'sqlalchemy'
