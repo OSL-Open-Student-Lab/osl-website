11b22e3cf7d2cee7f03f31fea2302c37f10048c3
timaracov <neyenburgz@mail.ru>
2022-02-05 18:29:27 +0200

    remove swagger stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from src import app
 from src.models import db
-from src.blueprints import auth, query, swagger
+from src.blueprints import auth, query
 from flask_session import Session
 
 db.init_app(app)
@@ -13,6 +13,5 @@ with app.app_context():
 if __name__ == '__main__':
     app.register_blueprint(auth.auth_bp)
     app.register_blueprint(query.query_bp)
-    app.register_blueprint(swagger.swagger_ui_bp, url_prefix=swagger.SWAGGER_URL)
-    
+  
     app.run(debug=True)
---
+++ src/blueprints/devices.py
@@ -0,0 +1,13 @@
+from crypt import methods
+from tempfile import TemporaryFile
+from flask.blueprints import Blueprint
+from src.models import Facilities, db
+
+devices_bp = Blueprint(name="devices", import_name=__name__, url_prefix="/api/v1/devices")
+
+@devices_bp.route("/", methods=["GET"])
+def get_device_list():
+    try:
+        device_list = db.session.query(Facilities)
+    except Exception as err:
+        print(err)
