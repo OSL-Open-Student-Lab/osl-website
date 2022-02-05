e5d39ae280f8dd18be06f11d431f4a3657897335
timaracov <neyenburgz@mail.ru>
2022-02-05 20:01:22 +0200

    transfer data to envs

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import datetime
 
 from src.models import db
 
-SECRET_KEY = 'dev_key'
+SECRET_KEY = os.getenv('OSL_SECRET_KEY')
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/site.db'
-SQLALCHEMY_DATABASE_URI = 'postgresql://lwhyaywahcvplq:cad4f8035162923ea3e2deeaa0ac233f9c921fc7eaaf3415cff72451895ea821@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dekaee32lbf2ro'
+SQLALCHEMY_DATABASE_URI = os.getenv('OSL_DB_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SESSION_TYPE = 'sqlalchemy'
