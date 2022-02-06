4f3f5b8574599bb4ffd50d95ca0b56a004434775
timaracov <neyenburgz@mail.ru>
2022-02-06 15:42:48 +0200

    add /apidocs route

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from src import app
 from src.models import db
 from src.blueprints import api
+
 from flask_session import Session
-
-
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app(testing=True):
     db.init_app(app)
@@ -13,5 +13,7 @@ def create_app(testing=True):
         db.create_all()
     
     app.register_blueprint(api.api_bp)
+    swagger_bp = get_swaggerui_blueprint('/apidocs', '/static/swagger.json')
+    app.register_blueprint(swagger_bp)
 
     return app
