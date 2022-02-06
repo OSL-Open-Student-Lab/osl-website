d2958b32d12bed4ff02d2a9ffabab37bb528f935
timaracov <neyenburgz@mail.ru>
2022-02-06 15:43:12 +0200

    add small changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_login import LoginManager
-from datetime import timedelta
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
--- src/blueprints/api.py
+++ src/blueprints/api.py
@@ -1,12 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory
 from src.blueprints import admin, auth, query
 
-api_bp = Blueprint(name="home", import_name=__name__, url_prefix="/api/v1/")
 
-api_bp.register_blueprint(auth.auth_bp)
-api_bp.register_blueprint(query.query_bp)
-api_bp.register_blueprint(admin.admin_bp)
+api_bp = Blueprint(name="home", import_name=__name__, url_prefix="/api/v1/")
 
 @api_bp.route('/', methods=["GET"])
 def index():
     return 'OSL Website API: V1.0'
+
+@api_bp.route('/static/<path:path>')
+def get_static(path):
+    return send_from_directory('static', path)
+
+
+
+api_bp.register_blueprint(auth.auth_bp)
+api_bp.register_blueprint(query.query_bp)
+api_bp.register_blueprint(admin.admin_bp)
