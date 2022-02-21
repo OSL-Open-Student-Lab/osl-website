ddbd3abbc4d30098f901237948c92e4c8b1a10cf
Timofey neyenburg <neyenburgz@mail.ru>
2022-02-21 21:52:08 +0200

    config.py updated

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from api.models import db
 from os import getenv
 
 SECRET_KEY = getenv('OSL_SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL').replace('postgres', 'postgresql')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SESSION_TYPE = 'sqlalchemy'
 SESSION_SQLALCHEMY = db
