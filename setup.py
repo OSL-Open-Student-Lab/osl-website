5f6556617b03160fe6c7567801d0913e0ebb6811
Timofey neyenburg <neyenburgz@mail.ru>
2022-03-11 23:50:56 +0200

    adding CORS: TEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from api import app
 from api.db import setup_db
 from api.blueprints import api
 from flask_swagger_ui import get_swaggerui_blueprint
-
+from flask_cors import CORS
 
 def create_app():
 
@@ -11,6 +11,8 @@ def create_app():
     except Exception as err:
         print(f"[ERROR]: Unable to setup database -> {err}")
     
+    CORS(app)
+
     app.register_blueprint(api.api_bp)
     swagger_bp = get_swaggerui_blueprint(
         '/api/v1/apidocs', 
