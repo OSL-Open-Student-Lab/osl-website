50003487aa838577df2cdd7c855186deef4b1c9d
Timofey neyenburg <neyenburgz@mail.ru>
2022-03-12 15:25:08 +0200

    testing cookies

--- config.py
+++ config.py
@@ -2,17 +2,23 @@ import datetime
 from api.models import db
 from os import getenv
 
-SERVER_NAME = "osl-apiv1.herokuapp.com"
-SESSION_COOKIE_DOMAIN = "osl-apiv1.herokuapp.com"
+
 SECRET_KEY = getenv('OSL_SECRET_KEY')
+
 STATIC_FOLDER = 'static'
+
 SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL').replace('postgres', 'postgresql')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SESSION_TYPE = 'sqlalchemy'
+
+SERVER_NAME = "osl-apiv1.herokuapp.com"
+
 SESSION_SQLALCHEMY = db
+SESSION_TYPE = 'sqlalchemy'
+SESSION_COOKIE_SAMESITE = "Strict"
+SESSION_COOKIE_HTTPONLY=True
+SESSION_COOKIE_NAME = 'osl-apiv1-session'
+SESSION_COOKIE_SECURE=False
+SESSION_COOKIE_DOMAIN = "osl-apiv1.herokuapp.com"
 PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=90)
 REMEMBER_COOKIE_DURATION=datetime.timedelta(days=1)
-SESSION_COOKIE_SECURE=False
-REMEMBER_COOKIE_HTTPONLY=True
-SESSION_COOKIE_SAMESITE='None'
-SESSION_COOKIE_NAME = 'osl-apiv1-session'
+
