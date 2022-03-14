f88a6cc7985444e447e38a2e9049015c19fbbc20
Timofey neyenburg <neyenburgz@mail.ru>
2022-03-14 22:53:27 +0200

    changin db and swagger

--- api/db.py
+++ api/db.py
@@ -8,29 +8,31 @@ def setup_db():
 
     with app.app_context():
         db.create_all()
+        any_roles = Roles.query.filter_by(id=1).all()
+        if any_roles:
+            return
+        else:
+            admin_role = Roles(
+              name="admin",
+              booking_access=True,
+              news_posting_access=True,
+              admin_access=True)
 
-        # Adding default roles
-        admin_role = Roles(
-          name="admin",
-          booking_access=True,
-          news_posting_access=True,
-          admin_access=True)
+            user_role = Roles(
+              name="user",
+              booking_access=False,
+              news_posting_access=False,
+              admin_access=False)
 
-        user_role = Roles(
-          name="user",
-          booking_access=False,
-          news_posting_access=False,
-          admin_access=False)
+            author_role = Roles(
+              name="author",
+              booking_access=False,
+              news_posting_access=True,
+              admin_access=False)
 
-        author_role = Roles(
-          name="author",
-          booking_access=False,
-          news_posting_access=True,
-          admin_access=False)
-
-        db.session.add(admin_role)
-        db.session.add(user_role)
-        db.session.add(author_role)
-        
-        db.session.commit()
+            db.session.add(admin_role)
+            db.session.add(user_role)
+            db.session.add(author_role)
+            
+            db.session.commit()
 
