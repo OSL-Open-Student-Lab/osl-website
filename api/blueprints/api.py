b432ba46e6f4867e8cb811adbfebaf47c9241270
timaracov <67540020+timaracov@users.noreply.github.com>
2022-03-20 13:12:52 +0200

    Update api.py

--- api/blueprints/api.py
+++ api/blueprints/api.py
@@ -3,10 +3,10 @@ from api.blueprints import auth, queue
 
 api_bp = Blueprint(name="home", import_name=__name__, url_prefix="/api/v1/")
 
+
 @api_bp.route('/static/<path:path>')
 async def get_static(path):
     return send_from_directory('static', path)
 
 api_bp.register_blueprint(auth.auth_bp)
 api_bp.register_blueprint(queue.queue_bp)
-
