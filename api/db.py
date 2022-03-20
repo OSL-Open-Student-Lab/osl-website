b54989874e475a05ae704272a936d375f26538a0
timaracov <67540020+timaracov@users.noreply.github.com>
2022-03-20 13:12:21 +0200

    Update db.py

--- api/db.py
+++ api/db.py
@@ -1,6 +1,7 @@
 from flask_session import Session
 from api.models import db, Roles
 
+
 def setup_db(app):
     db.init_app(app)
     Session(app)
@@ -30,6 +31,5 @@ def setup_db(app):
             db.session.add(admin_role)
             db.session.add(user_role)
             db.session.add(author_role)
-            
-            db.session.commit()
 
+            db.session.commit()
