452e5336fd528605bf95875e7c049e2ed15e7ac0
timaracov <neyenburgz@mail.ru>
2022-03-30 00:12:31 +0200

    adding validation for new feature

--- api/field_models.py
+++ api/field_models.py
@@ -95,3 +95,30 @@ class QueueField(BaseModel):
         if not valid:
             raise ValueError('Invalid date format(must be %d-%m-%Y %H:%M)')
         return dts
+
+
+class FacilityTypeField(BaseModel):
+    name: str
+    
+    @validator('name')
+    def valid_name(cls, name):
+        if name.isdigit():
+            raise ValueError('Name must contain characters')
+        return name
+
+class FacilityField(BaseModel):
+    name: str
+    type_id: int
+
+    @validator('name')
+    def valid_name(cls, name):
+        if name.isdigit():
+            raise ValueError('Name must contain characters')
+        return name
+
+    @validator('type_id')
+    def valid_id(cls, type_id):
+        if isinstance(type_id, float):
+            raise ValueError('Id must be integer')
+        return type_id
+
