e6d1cb1f0a7e834e5cadd35522d5bf72e530d2f6
timaracov <neyenburgz@mail.ru>
2022-04-17 18:29:20 +0200

    clearing fastapi branch

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_login import LoginManager
-
-app = Flask(__name__)
-app.config.from_pyfile('../config.py')
-
-lm = LoginManager()
-lm.session_protection = 'strong'
-lm.init_app(app)
